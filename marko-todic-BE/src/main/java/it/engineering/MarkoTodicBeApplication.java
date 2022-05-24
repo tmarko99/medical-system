abd2424b852eaf68fcd96dc302e0fcde0bfe9e72
Marko Todic <todic.marko99@gmail.com>
2022-05-24 13:24:58 +0200

    Added backend part of application

---
+++ marko-todic-BE/src/main/java/it/engineering/MarkoTodicBeApplication.java
@@ -0,0 +1,13 @@
+package it.engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkoTodicBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkoTodicBeApplication.class, args);
+	}
+
+}
---
+++ marko-todic-BE/src/test/java/it/engineering/MarkoTodicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkoTodicBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
